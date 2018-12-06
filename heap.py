1397e96d2449ab9944509aff8feb9e9dd0be9635
Brecht Ooms <brecht.ooms@hotmail.com>
2018-12-06 10:28:13 +0100

    heap WIP

---
+++ heap.py
@@ -0,0 +1,11 @@
+# Project Objectorientatie: NIM -  Heap classes
+# Brecht Ooms / 2 ELO-ICT / 2018
+
+import tkinter as tk
+
+
+class Heap(tk.Frame):
+    """Docstring for Heap class"""
+
+    def __init__(self, master=None):
+        super().__init__(master)
